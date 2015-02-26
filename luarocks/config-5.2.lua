26de52541d3b50264907666a1f8a18bbc6b30b22
daurnimator <quae@daurnimator.com>
2015-02-26 05:59:00 +0000

    luarocks/config-5.2: Add LUA variable that points to 5.2

--- luarocks/config-5.2.lua
+++ luarocks/config-5.2.lua
@@ -1,5 +1,6 @@
 export_lua_path = "export LUA_PATH_5_2='%s'";
 export_lua_cpath = "export LUA_CPATH_5_2='%s'";
 variables = {
+	LUA=[[/usr/bin/lua5.2]];
         LUA_INCDIR=[[/usr/include/lua5.2]];
 }
