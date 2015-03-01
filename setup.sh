f9a97eb8144d81a2d82d0138d4478892bbac7aa4
Lance Vick <lance@lrvick.net>
2015-03-01 10:36:25 +0000

    debugging setup.sh script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get install apt-transport-https
+apt-get install -y -q --force-yes apt-transport-https
 wget https://raw.githubusercontent.com/hashbang/shell-etc/master/apt/sources.list -O /etc/apt/sources.list
 wget https://raw.githubusercontent.com/hashbang/shell-etc/master/apt/preferences -O /etc/apt/preferences
 export DEBIAN_FRONTEND=noninteractive
@@ -10,14 +10,14 @@ apt-get upgrade -y -q --force-yes
 apt-get dist-upgrade -y -q --force-yes
 
 wget https://raw.githubusercontent.com/hashbang/shell-etc/master/packages.txt -O /tmp/packages.txt
-aptitude install $(cat /tmp/packages.txt | awk '{print $1}')
-#apt-get install -t unstable selinux-policy-default
+aptitude install -y -q $(cat /tmp/packages.txt | awk '{print $1}')
+#apt-get install -y -q -t unstable selinux-policy-default
 
-cd /etc
-git init
-git remote add origin https://github.com/hashbang/shell-etc.git
-git fetch --all
-git reset --hard origin/master
+#cd /etc
+#git init
+#git remote add origin https://github.com/hashbang/shell-etc.git
+#git fetch --all
+#git reset --hard origin/master
 
 #selinux-activate
 #reboot
