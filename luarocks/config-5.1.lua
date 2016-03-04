7e02361a1313ef6e3220e4bbd4892f0723b45a70
daurnimator <quae@daurnimator.com>
2016-03-04 11:33:34 +1100

    Fix luarocks configs

--- luarocks/config-5.1.lua
+++ luarocks/config-5.1.lua
@@ -1,4 +1,5 @@
-rocks_trees = {
-   home..[[/.luarocks]],
-   [[/usr/local]]
+variables = {
+	LUA_INTERPRETER = "/usr/bin/lua5.1";
+	LUA_INCDIR = "/usr/include/lua5.1";
 }
+rocks_subdir = "/lib/luarocks/rocks-5.1"
