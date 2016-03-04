ffbfab882a2e5acc57e869fa72fdcb4869cd7279
daurnimator <quae@daurnimator.com>
2016-03-04 01:10:00 +0000

    Add luarocks config for 5.3

---
+++ luarocks/config-5.3.lua
@@ -0,0 +1,7 @@
+export_lua_path = "export LUA_PATH_5_3='%s'";
+export_lua_cpath = "export LUA_CPATH_5_3='%s'";
+variables = {
+	LUA_INTERPRETER = "/usr/bin/lua5.3";
+	LUA_INCDIR = "/usr/include/lua5.3";
+}
+rocks_subdir = "/lib/luarocks/rocks-5.3"
