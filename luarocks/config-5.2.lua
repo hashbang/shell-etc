c2eab526a9897c5b0f80db3bba8f782e2af40300
daurnimator <quae@daurnimator.com>
2016-03-04 11:33:34 +1100

    Fix luarocks configs

--- luarocks/config-5.2.lua
+++ luarocks/config-5.2.lua
@@ -1,6 +1,7 @@
 export_lua_path = "export LUA_PATH_5_2='%s'";
 export_lua_cpath = "export LUA_CPATH_5_2='%s'";
 variables = {
-	LUA=[[/usr/bin/lua5.2]];
-        LUA_INCDIR=[[/usr/include/lua5.2]];
+	LUA_INTERPRETER = "/usr/bin/lua5.2";
+	LUA_INCDIR = "/usr/include/lua5.2";
 }
+rocks_subdir = "/lib/luarocks/rocks-5.2"
