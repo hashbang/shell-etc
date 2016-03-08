44aae1a451f1440f777d053e678c0c77941d062c
daurnimator <quae@daurnimator.com>
2016-03-08 11:36:00 +1100

    Have luarocks add to $PATH
    Also changes order; lua5.1 doesn't need to go last any more, as luarocks config now specifies version specific env vars.
    i.e. LUA_PATH_5_2

--- profile.d/luarocks_aliases.sh
+++ profile.d/luarocks_aliases.sh
@@ -1,8 +1,7 @@
 alias luarocks-5.1="lua5.1 /usr/bin/luarocks --local"
 alias luarocks-5.2="lua5.2 /usr/bin/luarocks --local"
 alias luarocks-5.3="lua5.3 /usr/bin/luarocks --local"
-eval `lua5.3 /usr/bin/luarocks path`
-eval `lua5.2 /usr/bin/luarocks path`
-eval `lua5.1 /usr/bin/luarocks path`
-# always do 5.1 last
-
+# In *reverse* order of PATH preference
+eval `lua5.1 /usr/bin/luarocks --bin path`
+eval `lua5.2 /usr/bin/luarocks --bin path`
+eval `lua5.3 /usr/bin/luarocks --bin path`
