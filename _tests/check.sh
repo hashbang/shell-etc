824eec8686c0a0091d57cf00cb088ecb83dc9941
The Fox in the Shell <KellerFuchs@hashbang.sh>
2017-01-10 20:04:04 +0100

    _tests/check: Tmp debug

--- _tests/check.sh
+++ _tests/check.sh
@@ -1,4 +1,7 @@
-#!/bin/sh -e
+#!/bin/sh -ex
+
+pwd
+git status
 
 # Apply the etckeeper state
 _tests/init.sh
@@ -6,6 +9,9 @@ _tests/init.sh
 # Update the etckeeper state
 # (LANG=C is required as locale influences sorting order)
 LANG=C etckeeper pre-commit -d .
+
+git diff --cached
+git status
 
 # Check that only ownership changed
 if git diff --cached | grep -E '^[+-][^+-]' | \
