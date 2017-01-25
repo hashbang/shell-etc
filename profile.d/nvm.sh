6fcf82e323f04270e328b471652daf5cd6cb9292
Danny Grove <danny@drgrovellc.com>
2017-01-24 17:48:55 -0800

    profile.d/nvm: remove excess bracket, use git -C instead of cd

--- profile.d/nvm.sh
+++ profile.d/nvm.sh
@@ -2,9 +2,8 @@ export NVM_DIR="$HOME/.nvm"
 
 install_nvm() {
   git clone https://github.com/creationix/nvm "$NVM_DIR"
-  cd "$NVM_DIR"
-  git checkout $(git describe --abbrev=0 --tags --match "v[0-9]*" origin)
+  git -C "$NVM_DIR" checkout $(git -C "$NVM_DIR" describe --abbrev=0 --tags --match "v[0-9]*" origin)
   . "$NVM_DIR/nvm.sh"
 }
 
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" ]
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
