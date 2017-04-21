3848c208da7e72e6218823429130a1031926d5ae
The Fox in the Shell <KellerFuchs@hashbang.sh>
2017-04-20 20:17:34 -0400

    nginx/symlinks: Fixup (do not create links for system users)

--- nginx/symlinks.sh
+++ nginx/symlinks.sh
@@ -4,6 +4,11 @@ UID=$(id -u "$USER")
 XDG_RUNTIME_DIR="/run/user/${UID}"
 NGINX_SOCK_DIR="/run/http"
 
+# Only proceed for human users: UID ranges 1000-60000 and 655536-4294967294
+([ "$UID" -ge 1000 ] && [ "$UID" -lt 60000 ])          || \
+    ([ "$UID" -ge 65536 ] && [ "$UID" -lt 4294967294 ]) || \
+    exit 0
+
 for proto in http https; do
     SOCKET_LINK="${NGINX_SOCK_DIR}/${USER}-${proto}.sock"
     [ -L "${SOCKET_LINK}" ] || \
