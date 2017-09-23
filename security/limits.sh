84dccc731987eb59806ba21a57f9eb1c2d1b5945
The Fox in the Shell <KellerFuchs@hashbang.sh>
2017-09-23 23:05:39 +0200

    security/limits.sh: Fixup
    The absolute path for systemctl was wrong...

--- security/limits.sh
+++ security/limits.sh
@@ -3,6 +3,6 @@
 PAM_UID=$(getent passwd "${PAM_USER}" | cut -d: -f3)
 
 if [ "${PAM_UID}" -ge 1000 ]; then
-    /usr/bin/systemctl set-property "user-${PAM_UID}.slice" \
-                       CPUQuota=50% MemoryLimit=512M BlockIOWeight=10
+    /bin/systemctl set-property "user-${PAM_UID}.slice" \
+                   CPUQuota=50% MemoryLimit=512M BlockIOWeight=10
 fi
